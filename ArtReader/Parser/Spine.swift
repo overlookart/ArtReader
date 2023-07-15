4719d2f817d1a541463c670be7c24e8393715ed0
丫丫的刀了 <598823426@qq.com>
2023-07-15 23:23:27 +0800

    spine 关联 resource

--- ArtReader/Parser/Spine.swift
+++ ArtReader/Parser/Spine.swift
@@ -10,16 +10,17 @@ import SwiftSoup
 struct Spine {
     struct SpineItem {
         var idref: String
+        var resource: Resource
     }
     var toc: String?
     var items: [SpineItem]?
     
-    init(element: Element) {
+    init(element: Element, manifest: [Resource]) {
         guard let items = try? element.getElementsByTag("itemref") else { return }
         self.items = []
         for item in items {
-            if let idref = try? item.attr("idref") {
-                self.items?.append(SpineItem(idref: idref))
+            if let idref = try? item.attr("idref"), let res = manifest.filter({ $0.id == idref }).first {
+                self.items?.append(SpineItem(idref: idref, resource: res))
             }
         }
     }
